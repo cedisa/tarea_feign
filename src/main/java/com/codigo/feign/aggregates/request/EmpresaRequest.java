e58c883f1fcdc1bb7840080ce21f71699c70eb49
cgamarraa <camilohidalgolara@gmail.com>
2024-02-10 00:34:56 -0500

    v1

---
+++ src/main/java/com/codigo/feign/aggregates/request/EmpresaRequest.java
@@ -0,0 +1,10 @@
+package com.codigo.feign.aggregates.request;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class EmpresaRequest {
+    private String tipoDoc;
+    private String numDoc;
+}
